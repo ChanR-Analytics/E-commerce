c1ae9a7f1676ff86a78a51e2fdaa317e174a692a
unknown <kaustubh.dwi@gmail.com>
2018-09-18 03:12:46 +0530

    update slug

--- products/views.py
+++ products/views.py
@@ -29,6 +29,22 @@ class Product_detail_Slug_view(DetailView):
     queryset = Product.objects.all()
     template_name ="products/detail.html"
 
+ #def get_object(self, *args, **kwargs):
+#         request= self.request
+#         slug= self.kwargs.get('slug')
+#         #instance=get_object_or_404(Product,slug=slug ,active=True)
+#         try:
+#             instance=Product.objects.get(slug=slug, active=True)
+#         except Product.DoesNotExist:
+#             raise Http404("not found")
+#         except Product.MultipleObjectsReturned:
+#             qs=Product.objects.filter(slug=slug, active=True)
+#             instance=qs.first()
+#         except:
+#             raise Http404("Ummmm")
+#         return instance
+#
+#
 
 
 
